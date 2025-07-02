8be17a80e7878b41cf96fa56f64baad70e4c6827
SugandhRanawatt <sugandhpatna95@gmail.com>
2025-07-02 22:20:18 +0530

    updated app icon image

--- data/user-data.js
+++ data/user-data.js
@@ -2,12 +2,12 @@ export const userData = {
   githubUser: 'Ranawatt',
   devUsername: "su_koder",
   github: 'https://github.com/Ranawatt',
-  facebook: 'https://www.facebook.com/abusaid.riyaz/',
+  facebook: 'https://www.facebook.com/',
   linkedIn: 'https://www.linkedin.com/in/sugandh-kumar-816181a1/',
   twitter: 'https://x.com/kumar_suga',
   stackOverflow: 'https://stackoverflow.com/users/7424149/sugandh-kumar',
   leetcode: "https://leetcode.com/u/su_koder/",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/1PA2J7lYmbTM6ex9coOgXn0boledW4U9L/view?usp=sharing",
   skills: ['Rust', 'Kotlin', 'C++', 'Java','React', 'NextJS', 'Redux', 'Express', 'NestJS', 'MySql', 'MongoDB', 'Postgres', 'Docker', 'AWS'],
   timezone: '+6'
 };
